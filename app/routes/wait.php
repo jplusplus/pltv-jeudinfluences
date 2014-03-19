cd161460fba52debf80f622bd3e334253639a061
pirhoo <pierre.romera@gmail.com>
2014-03-19 16:38:50 +0100

    Added wait controller

---
+++ app/routes/wait.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\routes;
+
+$app->get('/wait', function() use ($app) {
+    $app->view()->appendData(array('viewName'=>'Wait'));
+    $app->render('wait.twig');
+})->name('Wait');
