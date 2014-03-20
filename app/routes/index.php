ea0c7f6b3450834fb44b6dfbc5a9fd7529b28767
Edouard <edou4rd@gmail.com>
2014-03-20 18:27:52 +0100

    cleaned

--- app/routes/index.php
+++ app/routes/index.php
@@ -2,8 +2,6 @@
 namespace app\routes;
 
 $app->get('/', function() use ($app) {
-	// $app->view()->appendData(array('viewName'=>'Home'));
-	error_log($app->config("home_template"));
 	$app->render($app->config("home_template"));
 })->name('Home');
 
