6a4a6304f20eb9270ac68539d56614cb2bd527aa
Paul Joannon <paul.joannon@gmail.com>
2014-04-11 10:27:28 +0200

    add summary API functions

--- app/routes/api.php
+++ app/routes/api.php
@@ -167,6 +167,79 @@ $app->get('/api/plot', function() use ($app) {
 	return ok($plot);
 });
 
+$app->get('/api/summary', function() use ($app) {
+	/**
+	* Chapter must have a name like [0-9].json
+	*/
+	$params = $app->request()->params();
+	if (!isset($params['chapter'])) { return wrong(array('error' => 'chapter needed')); }
+	$asked_chapter = $params['chapter'];
+
+	// We retrieve the object in the database
+	$summary_in_db = R::findOne('summary');
+
+	// We retrieve the content of the .json files
+	$summary = \app\helpers\Game::getSummary();
+
+	// We check if the summary store in database is not too old
+	if (empty($summary_in_db) or time() - $summary_in_db['time'] > 1 * 60 * 60) {
+		if (empty($summary_in_db)) {
+			$summary_in_db = R::dispense('summary');
+		}
+
+		// We aggregate all responses
+		$all_choices = array();
+		foreach ($summary as $chapter => $choicesgroup) {
+			$all_choices[$chapter] = array();
+			foreach ($choicesgroup as $key => $value) {
+				$all_choices[$chapter][$key] = array(0, 0);
+			}
+		}
+		$all_careers = R::find('career');
+		foreach ($all_careers as $career) {
+			$_choices = json_decode($career['choices'], true);
+			foreach ($all_choices as $chapter => $choicesgroup) {
+				foreach ($choicesgroup as $choice => $choicearray) {
+					if (isset($_choices[$choice])) {
+						$all_choices[$chapter][$choice][$_choices[$choice]] += 1;
+					}
+				}
+			}
+		}
+
+		// We compute the percentages
+		foreach ($all_choices as &$choicegroup) {
+			foreach($choicegroup as &$choice) {
+				$total = $choice[0] + $choice[1];
+				if ($total > 0) {
+					$choice[0] = $choice[0] * 100 / $total;
+					$choice[1] = $choice[1] * 100 / $total;
+				}
+			}
+		}
+
+		// We store the updated summary in database
+		$summary_in_db['choices'] = json_encode($all_choices);
+		$summary_in_db['time'] = time();
+		R::store($summary_in_db);
+	}
+
+	// We inject the percentages in the returned object
+	$returned_summary = $summary[$asked_chapter];
+	$chapter_choices = json_decode($summary_in_db['choices'], true)[$asked_chapter];
+	foreach ($returned_summary as $choice_key => &$choice) {
+		foreach ($choice['options'] as $key => &$option) {
+			$option = array(
+				"title" => $option,
+				"percentage" => $chapter_choices[$choice_key][$key]
+			);
+		}
+	}
+
+	// Finally, we set the results in the $summary object and send it back
+	return ok($returned_summary);
+});
+
 # -----------------------------------------------------------------------------
 #
 #    SUBSCRIBE TO THE NEWSLETTER
