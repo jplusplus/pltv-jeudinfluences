474d9c90188e9a262b9151cc37bf70c33a41042b
Paul Joannon <paul.joannon@gmail.com>
2014-04-23 14:46:04 +0200

    fix what has been broken in /api/career/erase

--- app/routes/api.career.php
+++ app/routes/api.career.php
@@ -189,16 +189,16 @@ $app->post('/api/career/erase', function() use ($app) {
     $scene   = intval($since[1]);
 
     $filter_iter = function($var) use ($chapter, $scene){
-        $_chapter = split('\.', $var);
+        $var = split('\.', $var);
         $_chapter = intval($var[0]);
-        $_scene   = split('\.', $var);
         $_scene   = intval($var[1]);
         if ($_chapter > $chapter) { return false; }
         else if ($_chapter == $chapter && $_scene > $scene) { return false; }
         return true;
     };
-    
-    $career->scenes = array_values($career->scenes);
+
+    // Clean the scenes array
+    $career->scenes = array_filter($career->scenes, $filter_iter);
 
     // Clean the choices object
     $kept_choices = array_filter(array_keys($career->choices), $filter_iter);
