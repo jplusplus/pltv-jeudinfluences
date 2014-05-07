e6d5a9e95131e2b4ff8671b8f62d94b8094ed3c4
Paul Joannon <paul.joannon@gmail.com>
2014-05-07 15:42:14 +0200

    fix token filtering on /api/summary/final

--- app/routes/api.summary.php
+++ app/routes/api.summary.php
@@ -113,7 +113,7 @@ $app->get('/api/summary/final', function() use ($app) {
     $tokencondition = "";
     $params = $app->request()->params();
     if (isset($params['token'])) {
-        $tokencondition = " AND token <> " . $params['token'];
+        $tokencondition = " AND token <> '" . $params['token'] . "'";
     }
 
     $result = R::getAll("SELECT guilt, honesty FROM career WHERE finished = 1" . $tokencondition . " ORDER BY id DESC LIMIT 100");
